ccd929598304b81153b3f36b2b8a727dab3cee8b
JustSomeSoggybread <evanhlee@outlook.com>
2021-07-03 23:46:56 -0700

    check the comments

--- src/Lab6.cpp
+++ src/Lab6.cpp
@@ -11,9 +11,25 @@ void loop();
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
+/*
+  #include "oled-wing-adafruit.h"
+
+  OledWingAdafruit display;
+*/
+
 void setup() {
   Serial.begin(9600);
   pinMode(A4, INPUT);
+
+
+/*
+  display.setup();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+*/
+
 }
 
 void loop() {
@@ -25,10 +41,11 @@ void loop() {
   double temperature = (voltage - 0.5) * 100;
 
   Serial.println(temperature);
-  
+
   /*
-  uint64_t reading = analogRead(A4);
-  double voltage = (reading * 3.3) / 4095.0;
-  double temperature = (voltage - 0.5) * 100;
+  display.loop();
+  display.clearDisplay();
+  display.display();
+  display.println("Hello, world!");
   */
 }
